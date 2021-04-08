1b5fe096c9efe952c419ff6593657b5d114a1125
Changhyun Park <mattpark1028@gmail.com>
2021-04-08 15:49:06 +0900

    5-router-tutorial/2-parameter-query

--- 5-router-tutorial/src/About.js
+++ 5-router-tutorial/src/About.js
@@ -1,9 +1,16 @@
 import React from 'react';
-const About = () => {
+import qs from 'qs';
+
+const About = ({ location }) => {
+    const query = qs.parse(location.search, {
+        ignoreQueryPrefix: true
+    });
+    const detail = query.detail === 'true'
     return (
         <div>
             <h1>Intro.</h1>
             <p>this project is for pracwticing using react router</p>
+            {detail && <p>more info..</p>}
         </div>
     );
 };
