730843486d92a83aa6bc54d586a54eb50c36d4f6
Changhyun Park <mattpark1028@gmail.com>
2021-04-09 14:12:32 +0900

    Using redux dev tools

--- 6-redux/src/index.js
+++ 6-redux/src/index.js
@@ -2,11 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import { createStore } from 'redux';
+import { compose, createStore } from 'redux';
 import { Provider } from 'react-redux';
 import rootReducer from './modules';
+import { composeWithDevTools } from 'redux-devtools-extension';
 
-const store = createStore(rootReducer);//create store
+const store = createStore(rootReducer, composeWithDevTools());//create store
 
 ReactDOM.render(
 //wrapping it using Provider allows us to access redux store in any component
