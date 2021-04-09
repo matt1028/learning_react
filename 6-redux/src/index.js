a6f4a1722f28a940671fce9072dd8de47188fcbc
Changhyun Park <mattpark1028@gmail.com>
2021-04-09 13:50:18 +0900

    Redux module

--- 6-redux/src/index.js
+++ 6-redux/src/index.js
@@ -2,10 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import './exercise';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import rootReducer from './modules';
 
-ReactDOM.render(<App />,document.getElementById('root'));
+const store = createStore(rootReducer);//create store
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+ReactDOM.render(
+//wrapping it using Provider allows us to access redux store in any component
+<Provider store={store}>
+    <App />
+</Provider>,document.getElementById('root'));
